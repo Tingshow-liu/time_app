5f1c9c255923c88e7aed59009c000386be08e3a9
劉庭秀 <tingshow_liu@MacBook-Pro.local>
2023-09-27 23:14:44 -0400

    lab2

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
+
+@app.route('/time')
+def time():
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return current_time
 
 
 @app.route('/')
